f2cbe8e56b11f1f5d648d47c5c1e4c22af70f1eb
Dennis K <golden_talon@hotmail.de>
2019-02-10 08:26:12 +0100

    Fertig

---
+++ EvenChecker/EvenChecker/EvenChecker.cpp
@@ -0,0 +1,26 @@
+/*Write a program that asks the user to input an integer, 
+and tells the user whether the number is even or odd. 
+Write a function called isEven() that returns true if an integer passed to it is even. 
+Use the modulus operator to test whether the integer parameter is even.*/
+#include <iostream>
+
+bool isEven(int zahlEingabe)
+{
+	return zahlEingabe % 2;
+}
+
+int main()
+{
+	std::cout << "Gebe eine Zahl ein: ";
+	int zahlEingabe = 0;
+	std::cin >> zahlEingabe;
+	bool Auswertung= isEven(zahlEingabe);
+	if (Auswertung == false)
+	{
+		std::cout << "Zahl ist gleich!\n";
+	}
+	if (Auswertung == true)
+	{
+		std::cout << "Zahl ist ungleich!\n";
+	}
+}
